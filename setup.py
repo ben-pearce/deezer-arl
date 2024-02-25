70a2bcb7973d2530cb03657827c24b44feacf0a5
Ben Pearce <me@benpearce.io>
2024-02-25 13:44:56 +0000

    chore: :bookmark: bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deezer-arl',
-    version='1.0.2',
+    version='1.0.3',
     description='Deezer ARL Scraper',
     author='Ben Pearce',
     author_email='me@benpearce.io',
